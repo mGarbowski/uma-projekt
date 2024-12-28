20972fa439a34010487605b8f08d0a73d64d7870
mgarbowski <mikolaj.garbowski@gmail.com>
2024-12-28 14:49:26 +0100

    ensure the classifier works on all datasets

--- src/main.py
+++ src/main.py
@@ -5,16 +5,22 @@ from dataset.dataset import Dataset
 from evaluation.confusion_matrix import ConfusionMatrix
 
 
+def test_on_dataset(dataset: Dataset):
+    model = ID3Classifier.train(dataset)
+    predictions = model.predict(dataset.attributes)
+    actual_labels = dataset.labels
+    confusion_matrix = ConfusionMatrix.from_labels(actual_labels, predictions)
+    pprint(confusion_matrix.matrix)
+
+
 def main():
     car_evaluation = Dataset.load_from_file("datasets/car+evaluation/car.data")
     nursery = Dataset.load_from_file("datasets/nursery/nursery.data")
     balance_scale = Dataset.load_from_file("datasets/balance+scale/balance-scale.data")
 
-    model = ID3Classifier.train(car_evaluation)
-    predictions = model.predict(car_evaluation.attributes)
-    actual_labels = car_evaluation.labels
-    confusion_matrix = ConfusionMatrix.from_labels(actual_labels, predictions)
-    pprint(confusion_matrix.matrix)
+    test_on_dataset(car_evaluation)
+    test_on_dataset(nursery)
+    test_on_dataset(balance_scale)
 
 
 if __name__ == '__main__':
