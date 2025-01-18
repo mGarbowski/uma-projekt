d135f3964bd3920b51ffca06e0962efd8d40351b
mgarbowski <mikolaj.garbowski@gmail.com>
2025-01-18 19:39:58 +0100

    tests for OVO full coverage

--- tests/classifiers/test_one_vs_one.py
+++ tests/classifiers/test_one_vs_one.py
@@ -1,4 +1,6 @@
+from src.classifiers.id3 import ID3Classifier
 from src.classifiers.one_vs_one import get_pairs, OneVsOneClassifier
+from src.dataset.dataset import Dataset
 from tests.utils import given_tree_that_predicts
 
 
@@ -11,13 +13,28 @@ def test_get_pairs():
     assert ("B", "C") in pairs or ("C", "B") in pairs
 
 
-def test_ovo_predicts_label_with_highest_weighted_vote():
-    tree_ab = given_tree_that_predicts("A", 0.99)
-    tree_bc = given_tree_that_predicts("B", 0.8)
-    tree_ac = given_tree_that_predicts("C", 0.07)
-    ovo_model = OneVsOneClassifier(
-        {("A", "B"): tree_ab, ("B", "C"): tree_bc, ("A", "C"): tree_ac},
-        ("A", "B", "C")
-    )
+class TestOneVsOneClassifier:
+    def test_name(self):
+        assert OneVsOneClassifier.name() == "One-vs-One"
 
-    assert ovo_model.predict_single(("blah", "blah", "blah")) == "A"
+    def test_predicts_label_with_highest_weighted_vote(self):
+        tree_ab = given_tree_that_predicts("A", 0.99)
+        tree_bc = given_tree_that_predicts("B", 0.8)
+        tree_ac = given_tree_that_predicts("C", 0.07)
+        ovo_model = OneVsOneClassifier(
+            {("A", "B"): tree_ab, ("B", "C"): tree_bc, ("A", "C"): tree_ac},
+            ("A", "B", "C")
+        )
+
+        assert ovo_model.predict_single(("blah", "blah", "blah")) == "A"
+
+    def test_creates_trees_for_each_pair_of_classes(self):
+        dataset = Dataset(
+            [("A", "1"), ("B", "1"), ("B", "2"), ("B", "2"), ("B", "3")],
+            ["0", "1", "2", "0", "1"]
+        )
+        ovo_model = OneVsOneClassifier.train(dataset)
+
+        assert len(ovo_model._trees) == 3
+        assert len(get_pairs(dataset.unique_labels())) == 3
+        assert all(isinstance(tree, ID3Classifier) for tree in ovo_model._trees.values())
